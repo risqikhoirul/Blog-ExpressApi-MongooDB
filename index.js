0e4ac62ac4affa23bc01609feb05ccf40c7332f6
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-01-23 21:12:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// CONFIG CORS
-const corsOptions = {
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"], // PLEASE CHANGE
-  optionsSuccessStatus: 200,
-};
-// END
+const { corsOptions } = require("./config/cors.config");
 
 app.use(cors(corsOptions));
 app.use(express.json());
