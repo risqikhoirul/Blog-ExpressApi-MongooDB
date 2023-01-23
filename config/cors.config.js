a1bd7d9f2f8a552e1a6fc9687c4718f1aa249c5d
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-01-23 21:22:25 +0700

    Create cors.config.js

---
+++ config/cors.config.js
@@ -0,0 +1,8 @@
+// CONFIG CORS
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000"], // PLEASE CHANGE
+  optionsSuccessStatus: 200,
+};
+// END
+
+module.exports = corsOptions;
